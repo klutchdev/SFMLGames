077624cda70f2768bdccd679c99391157cbce6ce
Kyle 'Klutch' Leary <klutchdevelopment@gmail.com>
2022-07-05 05:14:33 -0400

    add Animation class

--- OPEN_WORLD_RPG/AnimationComponent.cpp
+++ OPEN_WORLD_RPG/AnimationComponent.cpp
@@ -1,9 +1,14 @@
 #include "AnimationComponent.h"
 
-AnimationComponent::AnimationComponent()
+AnimationComponent::AnimationComponent(sf::Sprite &sprite, sf::Texture &texture_sheet)
+    : sprite(sprite), textureSheet(texture_sheet)
 {
 }
 
 AnimationComponent::~AnimationComponent()
+{
+}
+
+void AnimationComponent::update(const float &dt)
 {
 }
