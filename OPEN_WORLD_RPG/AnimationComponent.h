dead60860761b175867f080fdb3eb28204d8b3bd
Kyle 'Klutch' Leary <klutchdevelopment@gmail.com>
2022-07-05 14:53:10 -0400

    Add animation component

--- OPEN_WORLD_RPG/AnimationComponent.h
+++ OPEN_WORLD_RPG/AnimationComponent.h
@@ -31,54 +31,56 @@ private:
 
         Animation(sf::Sprite &sprite, sf::Texture &texture_sheet,
                   float animation_timer,
-                  int start_x, int start_y,
-                  int end_x, int end_y,
+                  int start_frame_x, int start_frame_y,
+                  int frames_x, int frames_y,
                   int width, int height)
             : sprite(sprite), textureSheet(texture_sheet),
               animationTimer(animation_timer), width(width), height(height)
         {
-            this->startRect = sf::IntRect(start_x, start_y, width, height);
+            this->startRect = sf::IntRect(start_frame_x * width, start_frame_y, width, height);
             this->currentRect = startRect;
-            this->endRect = sf::IntRect(end_x, end_y, width, height);
+            this->endRect = sf::IntRect(frames_x * width, frames_y * height, width, height);
 
             this->sprite.setTexture(this->textureSheet, true);
             this->sprite.setTextureRect(this->startRect);
         }
 
-        void update(const float &dt)
+        void play(const float &dt)
         {
             // Update timer
             this->timer = 10.f * dt;
             if (this->timer >= this->animationTimer)
             {
-                // Reset timer
-                this->timer = 0.f;
+                this->timer = 0.f; // Reset timer
 
                 if (currentRect != this->endRect)
                     this->currentRect.left += this->width;
                 else
                     this->currentRect.left = this->startRect.left;
+
+                this->sprite.setTextureRect(this->currentRect);
             }
         }
 
-        void pause();
-        void reset();
+        void reset()
+        {
+            this->timer = 0.f;
+            this->currentRect = this->startRect;
+        }
     };
 
     sf::Sprite &sprite;
     sf::Texture &textureSheet;
-    std::map<std::string, Animation>
+    std::map<std::string, Animation *>
         animations;
 
 public:
     AnimationComponent(sf::Sprite &sprite, sf::Texture &texture_sheet);
     virtual ~AnimationComponent();
 
-    void addAnimation(const std::string key);
+    void addAnimation(const std::string key, sf::Sprite &sprite, sf::Texture &texture_sheet,
+                      float animation_timer, int start_frame_x, int start_frame_y,
+                      int frames_x, int frames_y, int width, int height);
 
-    void startAnimation(const std::string animation);
-    void pauseAnimation(const std::string animation);
-    void resetAnimation(const std::string animation);
-
-    void update(const float &dt);
+    void play(const std::string key, const float &dt);
 };
